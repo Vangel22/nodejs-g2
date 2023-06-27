4cbed39e29752af1098a7933568f50b161c0d6a1
Vangel22 <h.vangel22@gmail.com>
2023-06-27 22:09:55 +0200

    Homework save

--- c09/index.js
+++ c09/index.js
@@ -19,3 +19,9 @@ app.listen(8080, (err) => {
   if (err) console.log(err);
   console.log("Server started");
 });
+
+//Homework
+// 1. test-hw -> change html files with ejs
+// 2. Make the configuration in index.js -> app.set -> for ejs to be able to work with your app
+// 3. res.render(analiza)
+// 4. res.render(analiza-form)
