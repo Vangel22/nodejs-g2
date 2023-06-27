e4cb425bd53377e83937f519ed0fdbb913931cd4
Vangel22 <h.vangel22@gmail.com>
2023-06-27 21:44:20 +0200

    save for practise

--- c09/index.js
+++ c09/index.js
@@ -6,15 +6,9 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   let data = {
-    ime: "Pero",
-    prezime: "Perovski",
-    niza: ["Skopje", "Bitola", "Strumica"],
-    studenti: [
-      { ime: "Petre", prezime: "Petrevski", prosek: 9.2 },
-      { ime: "Ivan", prezime: "Ivanovski", prosek: 8.2 },
-      { ime: "Ace", prezime: "Acevski", prosek: 9.9 },
-      { ime: "Stanko", prezime: "Stankovski", prosek: 7.2 },
-    ],
+    fullName: "Student name", //change this
+    array: [""],
+    books: [{ author: "", year: 2012 }],
   };
   res.render("index", data);
 });
